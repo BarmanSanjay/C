f149f11eabdf40842b20dbe86eb66bd54fb37e60
BarmanSanjay <sanjay1341999@gmail.com>
2024-12-31 00:52:21 +0530

    Hello World!

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * @author sanjay
+ * @date 31/12/2024
+ * 
+ * Print "Hello, World!" to the console
+ */
+int main(void){
+    printf("Hello, World!\n");
+    return 0;
+}
