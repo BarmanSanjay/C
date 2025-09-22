df1db2b9661e11de8de172573bd704f3377485ce
sanjay <sanjay1341999@gmail.com>
2025-09-22 22:55:49 +0530

    segregate 0s and 1s

--- DSA/segregate_0s_1s.c
+++ DSA/segregate_0s_1s.c
@@ -17,12 +17,13 @@ void segregate0and1(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {0, 1, 0, 1, 1, 1};
+    int arr[] = {0, 1, 0, 1, 0, 1, 1, 1};// after segregate -> 0, 0, 0, 1, 1, 1, 1, 1
     int n = sizeof(arr) / sizeof(arr[0]);
     segregate0and1(arr, n);
     printf("Array after segregation: ");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
+    printf("\n");
     return 0;
 }
